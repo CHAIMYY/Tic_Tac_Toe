528339fb6b7e34b23de6074110b9505b3a8b72f6
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2024-08-30 12:23:50 +0100

    added table created with js and modifed css and html

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+const table = document.getElementById('gridTable');
 
-
-
-
+for (let i = 0; i < 20; i++) {
+    const row = table.insertRow(); 
+    for (let j = 0; j < 20; j++) {
+        const cell = row.insertCell();  
+        cell.textContent = '';
+       
+    }
+}
 
 
 
