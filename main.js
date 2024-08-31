2d19453807f0520bd13119c9814555e55740d01f
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2024-09-01 00:42:50 +0100

    Feat: Add player turn functionality

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
 const table = document.getElementById('gridTable');
+const restartBtn = document.getElementById('restart');
+const statusText = document.getElementById('status');
+let turn = 'x';
+
 
 for (let i = 0; i < 20; i++) {
     const row = table.insertRow(); 
     for (let j = 0; j < 20; j++) {
-        const cell = row.insertCell();  
+        let cell = row.insertCell();  
         cell.textContent = '';
+
+        cell.addEventListener('click', function() {
+           
+            console.log(`Cell clicked: Row ${i + 1}, Column ${j + 1}`);
+            
+           
+        
+
+            if(turn === 'x' && cell.textContent === ''){
+              cell.innerHTML = 'x';
+              turn = 'o';
+              statusText.textContent = `Player ${turn}'s turn`;
+
+            }else if(turn === 'o' && cell.textContent === ''){
+                cell.innerHTML = 'o';
+                turn = 'x';
+                statusText.textContent = `Player ${turn}'s turn`;
+
+            }else{
+               
+                statusText.textContent = `Player ${turn}'s turn`
+
+            }
+
+
+
+        });
+   
        
     }
+    
 }
+
 
 
 
